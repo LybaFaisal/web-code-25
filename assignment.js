d694f55dc7bc05cdee0ea6c4446542901a5b4dfe
Lyba <faisallyba06@gmail.com>
2024-01-12 21:08:38 +0500

    Code10

--- assignment.js
+++ assignment.js
@@ -123,3 +123,11 @@ for( let i=1; i<=5; i++)
     console.log(`iteration= ${i}`);
 }
 console.log("Iterations completed");
+
+//code 10
+
+for( let i=5; i>=1; i--)
+{
+    console.log(`iteration= ${i}`);
+}
+console.log("Counting Finished");
