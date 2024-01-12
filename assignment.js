6745436303ab61b3b4d258a63be29461e480666c
Lyba <faisallyba06@gmail.com>
2024-01-12 20:55:45 +0500

    Code2

--- assignment.js
+++ assignment.js
@@ -3,3 +3,11 @@
 let score = 8;
 
 console.log("Mid Level Skills:" + (score > 0 && score < 10))
+
+
+//code#2
+
+let timeRemaining = 0;
+let energy = 10;
+
+console.log(`Game Over: ${timeRemaining == 0 || energy == 0}`)
