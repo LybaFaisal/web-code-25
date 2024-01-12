c9433059a6e11bbd0f934ad2652c4f3a303424d4
Lyba <faisallyba06@gmail.com>
2024-01-12 21:04:43 +0500

    Code8

--- assignment.js
+++ assignment.js
@@ -68,3 +68,50 @@ else if (age < 18) {
 else {
     console.log("The value of the age variable is not numerical");
 }
+
+//code#8
+
+let day = "Sunday";
+
+switch (day) {
+    case "Monday":
+        {
+            console.log("Today is Monday");
+            break;
+        }
+    case "Tuesday":
+        {
+            console.log("Today is Tuesday");
+            break;
+        }
+    case "Wednesday":
+        {
+            console.log("Today is Wednesday");
+            break;
+        }
+    case "Thursday":
+        {
+            console.log("Today is Thursday");
+            break;
+        }
+    case "Friday":
+        {
+            console.log("Today is Friday");
+            break;
+        }
+    case "Saturday":
+        {
+            console.log("Today is Saturday");
+            break;
+        }
+    case "Sunday":
+        {
+            console.log("Today is Sunday");
+            break;
+        }
+    default:
+        {
+            console.log("invalid day");
+            break;
+        }
+}
